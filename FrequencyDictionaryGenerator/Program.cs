72967b4089609a0e093b6de493af66f2a4548547
jay <jay@C40LAP-15236>
2023-04-03 17:06:44 +0530

    Add project files.

---
+++ FrequencyDictionaryGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using FrequencyDictionaryGenerator;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+class FrequencyGenrator
+{
+    public  static void Main(string[] args) 
+    {
+        
+        Console.WriteLine("Please enter incoming file path");
+        //C:\Users\jay\Desktop\TestCaseText.txt
+        //C:\Users\jay\Desktop\ResultText.txt
+        string incoming_file_path = Console.ReadLine();
+        Console.WriteLine("Please enter result file path");       
+        string result_file_path = Console.ReadLine();                
+        Utility.GetFrequancy(incoming_file_path, result_file_path);
+        Console.ReadKey();
+        Console.ReadLine();
+
+    }
+}
---
+++ FrequencyDictionaryGenerator/Utility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyDictionaryGenerator
+{
+    internal static class Utility
+    {
+        public static void GetFrequancy(string incoming_file_path, string result_file_path)
+        {
+
+            StreamReader inputFile;
+            string words = string.Empty;
+            try
+            {
+                if (File.Exists(incoming_file_path))
+                {
+                    inputFile = new StreamReader(incoming_file_path);
+                    words = inputFile.ReadLine();
+                }
+
+                var dict = new Dictionary<string, int>();
+                foreach (var word in words.Split(' '))
+                {
+                    if (dict.ContainsKey(word))
+                        dict[word]++;
+                    else
+                        dict.Add(word, 1);
+
+                }
+                foreach (var e in dict)
+                {
+                    Console.WriteLine($"{e.Key}){e.Value}");
+                    File.WriteAllLines(result_file_path,
+                   dict.Select(x => "[" + x.Key + " " + x.Value + "]").ToArray());
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+
+        }
+    }
+}
